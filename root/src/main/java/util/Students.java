45be3d9a16036094cb2a342f2efe38638818f77e
Humza Shahid <humzasaur@gmail.com>
2022-11-04 15:35:31 +0000

    implement four methods

--- root/src/main/java/util/Students.java
+++ root/src/main/java/util/Students.java
@@ -10,6 +10,12 @@ import static java.util.Comparator.*;
 import static java.util.Collections.*;
 import static java.util.Arrays.*;
 
+// I was originally going to use an SQLite ORM 
+// and create a dependency-injected repository class to persist students,
+// as task expectations were not 100% clear, but the method signatures look like
+// they expect application logic and I personally think that coding
+// a sorting algorithm like quicksort/merge sort is more impressive 
+// than sorting by passing over an SQL string. So no data persistence.
 
 public class Students {
 
@@ -29,28 +35,64 @@ public class Students {
         return new Students(students);
 
     }
+    
+    // Just an abstraction for the first two methods, which use mostly the same logic.
+    // I am aware of the disadvantages of primitive obsession and passing year
+    // as an integer, but I don't think it's a problem in this task.
+    private boolean yearBetween(int from, int to, int year) {
+    	if (year >= from && year <= to) {
+    		return true;
+    	}
+    	return false;
+    }
 
      // Return all students that start(ed) between the the two
      // given years (inclusive of from and to)
     public Collection<Student> startingBetween(int from, int to) {
-
-        return emptyList(); // todo: implement
-
+    	ArrayList<Student> result = new ArrayList<Student>();
+    	for (Student student : students) {
+    		if (yearBetween(from, to, student.getStartYear().getStart()))
+    		{
+    			result.add(student);
+    		}
+    	}
+        return result;
     }
 
      // Return all students that finish(ed) between the the two given
      // years (inclusive of from and to).
     public Collection<Student> finishingBetween(int from, int to) {
-
-        return emptyList(); // todo: implement
-
+    	ArrayList<Student> result = new ArrayList<Student>();
+    	for (Student student : students)
+    	{
+    		if (yearBetween(from, to, student.getEndYear().getEnd())) {
+    			result.add(student);
+    		}
+    	}
+        return result;
     }
 
      // Return all students grouped by start year.
      // The result should be a Map from school year to a collection of students.
     public Map<SchoolYear, ? extends Collection<Student>> groupedByStartYear() {
-
-        return emptyMap(); // todo: implement
+    	// I appreciate the generated equals and hash code override provided here for the school year.
+    	// I have had a growing appreciation of immutability after learning functional programming,
+    	// and I know by default objects would be compared by reference rather than value,
+    	// which would make this problem a little more difficult since I would need to either override
+    	// the default SchoolYear equals/compare or use a primitive.
+    	
+    	Map<SchoolYear, ArrayList<Student>> result = new HashMap<SchoolYear, ArrayList<Student>>();
+    	
+    	for (Student student : students)
+    	{
+    		SchoolYear year = student.getStartYear();
+    		if (!result.containsKey(year)) {
+    			result.put(year, new ArrayList<Student>());
+    		}
+    		result.get(year).add(student);
+    	}
+    	
+        return result;
 
     }
 
